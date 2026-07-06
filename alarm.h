e6df3791795f0303ca10656e5a5dbeca29471b4d
Park Seohee <142740663+seoeeh@users.noreply.github.com>
2026-07-06 00:56:23 +0000

    줄바꿈

--- alarm.h
+++ alarm.h
@@ -11,8 +11,13 @@ namespace ParkSeohee2114012
         alarm(timeOfDay t = timeOfDay{1,1}, bool a = false):wakeTime{t}, inActive(a){} 
         void print() 
         {
-            wakeTime.print();
-            std::cout << " alarm is " << ((inActive) ? "on" : "off") << "\n"; //ifelse구문으로해도딤
+          if (wakeTime.getHour() < 10) std::cout << "0";
+    std::cout << wakeTime.getHour() << ":"; 
+    
+    if (wakeTime.getMinute() < 10) std::cout << "0";
+    std::cout << wakeTime.getMinute(); 
+
+    std::cout << " alarm is " << ((inActive) ? "on" : "off") << "\n"; //ifelse구문으로해도딤
         }
         const timeOfDay& getWakeTime() const { return wakeTime;}
         void setWakeTime(const timeOfDay& t) { wakeTime = t; }
